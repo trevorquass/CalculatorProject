163a07fe84dd637b4b5eb3824d8d205c73183a6a
unknown <nkindschi@dev3.bsecode.inc>
2015-09-23 17:50:50 -0500

    Added test file

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AdditionOperator testAdditionOperator = new AdditionOperator();
+            testAdditionOperator.AddInput();
+            int expected = 8;
+            int actual = result.AddInput();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
